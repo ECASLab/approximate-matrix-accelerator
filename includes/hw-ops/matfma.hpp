4ebd9f42b2d837ff1a3f5b98471b66ddd1cceec7
Eduardo Salazar <eduarsalazar96@gmail.com>
2021-10-06 14:22:32 -0600

    Optimazing matfma

--- includes/hw-ops/matfma.hpp
+++ includes/hw-ops/matfma.hpp
@@ -8,6 +8,18 @@
 
 namespace ama {
 namespace hw {
+
+template <typename T>
+/**
+ * @brief Multiply
+ * @param a Left parameter to multiply
+ * @param b Right parameter to multiply
+ */
+static T mulfma(const T a, const T b){
+#pragma HLS INLINE off
+	return a * b;
+}
+
 template <typename T, int M, int N>
 /**
  * @brief Fused Multiply-Add
@@ -18,12 +30,16 @@ template <typename T, int M, int N>
  * @param res Matrix with the result
  */
 void matfma(const T a[M][N], const T b[N][M], const T c[M][M], T res[M][M]) {
-  for (int i = 0; i < M; i++) {
-    for (int j = 0; j < M; j++) {
-      res[i][j] = c[i][j];
-      for (int k = 0; k < N; k++) {
-        res[i][j] += a[i][k] * b[k][j];
+  T tmp = 0;
+  Rows: for (int i = 0; i < M; i++) {
+    Cols: for (int j = 0; j < M; j++) {
+#pragma HLS PIPELINE II = 2
+      tmp = c[i][j];
+      //res[i][j] = c[i][j];
+      Res: for (int k = 0; k < N; k++) {
+    	tmp += mulfma(a[i][k], b[k][j]);
       }
+      res[i][j] = tmp;
     }
   }
 }
--- test/hw-ops/tops/matfma_top_accel.cpp
+++ test/hw-ops/tops/matfma_top_accel.cpp
@@ -4,13 +4,16 @@
  * Supervisor: Luis G. Leon-Vega <lleon95@estudiantec.cr>
  */
 
-#include "matfma_top_accel.hpp"
-
 #include "linear.hpp"
+#include "matfma_top_accel.hpp"
 
 void matfma_top_accel(const ExactType a[kRows][kCols],
                       const ExactType b[kCols][kRows],
                       const ExactType c[kRows][kRows],
                       ExactType res[kRows][kRows]) {
+#pragma HLS ARRAY_RESHAPE variable=res complete dim=2
+#pragma HLS ARRAY_RESHAPE variable=c complete dim=2
+#pragma HLS ARRAY_RESHAPE variable=b complete dim=1
+#pragma HLS ARRAY_RESHAPE variable=a complete dim=2
   ama::hw::matfma<ExactType, kRows, kCols>(a, b, c, res);
 }
