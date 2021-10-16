b92f8a2d90e7b21860dd45d354e8e7d88d145fa1
Eduardo Salazar <eduarsalazar96@gmail.com>
2021-10-16 00:32:07 -0600

    Optimizing matfma by adding the load_matrix_fma function for buffering the input matrices. Also, the RESHAPE pragma was changed for PARTITION pragma. The results reached are:
    7.906 ns of latency
    94 clocks cycles
    8% LUTs utilization
    2% FFs utilization

--- includes/hw-ops/matfma.hpp
+++ includes/hw-ops/matfma.hpp
@@ -9,6 +9,19 @@
 namespace ama {
 namespace hw {
 
+template <typename T, int M, int N>
+
+void load_matrix_fma(const T fifo[M][N], T reg[M][N]){
+//#pragma HLS ARRAY_PARTITION variable=fifo complete dim=0
+#pragma HLS INLINE off
+//#pragma HLS PIPELINE
+  for(int i = 0; i < M; i++){
+    for(int j = 0; j < N; j++){
+      reg[i][j] = fifo[i][j];
+    }
+  }
+}
+
 template <typename T>
 /**
  * @brief Multiply
@@ -36,16 +49,37 @@ template <typename T, int M, int N>
  * @param res Matrix with the result
  */
 void matfma(const T a[M][N], const T b[N][M], const T c[M][M], T res[M][M]) {
+#pragma HLS INTERFACE ap_fifo port=a
+#pragma HLS INTERFACE ap_fifo port=b
+#pragma HLS INTERFACE ap_fifo port=c
+#pragma HLS INTERFACE ap_fifo port=res
+#pragma HLS ARRAY_PARTITION variable=res complete dim=2
+/*
+#pragma HLS ARRAY_RESHAPE variable=res complete dim=2
+#pragma HLS ARRAY_RESHAPE variable=c complete dim=2
+#pragma HLS ARRAY_RESHAPE variable=b complete dim=0
+#pragma HLS ARRAY_RESHAPE variable=a complete dim=2
+*/
+T a_buff[M][N];
+#pragma HLS ARRAY_PARTITION variable=a_buff complete dim=0
+T b_buff[N][M];
+#pragma HLS ARRAY_PARTITION variable=b_buff complete dim=0
+T c_buff[M][M];
+#pragma HLS ARRAY_PARTITION variable=c_buff complete dim=2
+
   // T a_row[M];
   // T b_copy[N][M];
   T tmp = 0;
+load_matrix_fma<T,M,N>(a,a_buff);
+load_matrix_fma<T,N,M>(b,b_buff);
+load_matrix_fma<T,M,M>(c,c_buff);
 Rows:
   for (int i = 0; i < M; i++) {
 #pragma HLS PIPELINE
   Cols:
     for (int j = 0; j < M; j++) {
       //#pragma HLS PIPELINE
-      tmp = c[i][j];
+      tmp = c_buff[i][j];
     /*
           if (j == 0){
               Cache_Row: for(int k = 0; k < M; k++){
@@ -61,11 +95,12 @@ Rows:
     */
     Res:
       for (int k = 0; k < N; k++) {
-        tmp += mulfma(a[i][k], b[k][j]);
+        tmp += mulfma(a_buff[i][k], b_buff[k][j]);
       }
       res[i][j] = tmp;
     }
   }
+
 }
 }  // namespace hw
 }  // namespace ama
