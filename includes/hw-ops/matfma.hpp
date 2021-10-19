26ee3d0465ed8957333453dcd7de3b9d6f0ef8ca
Eduardo Salazar <eduarsalazar96@gmail.com>
2021-10-19 00:53:27 -0600

    Adding some comments

--- includes/hw-ops/matfma.hpp
+++ includes/hw-ops/matfma.hpp
@@ -30,6 +30,7 @@ template <typename T, int M, int N>
 void matfma(const T a[M][N], const T b[N][M], const T c[M][M], T res[M][M]) {
 // Uncomment the following section for not
 // register parallelism scenario (scenario 1)
+
 #pragma HLS INTERFACE ap_fifo port = a
 #pragma HLS INTERFACE ap_fifo port = b
 #pragma HLS INTERFACE ap_fifo port = c
@@ -46,9 +47,9 @@ void matfma(const T a[M][N], const T b[N][M], const T c[M][M], T res[M][M]) {
   load_matrix<T, N, M>(b, b_buff);
   load_matrix<T, M, M>(c, c_buff);
 
-// Uncomment the following section for scenario complete
-// register parallelism scenario (scenario 2)
-/*
+  // Uncomment the following section for scenario complete
+  // register parallelism scenario (scenario 2)
+  /*
 #pragma HLS INTERFACE register port=a
 #pragma HLS ARRAY_PARTITION variable=a complete dim=0
 #pragma HLS INTERFACE register port=b
@@ -57,23 +58,25 @@ void matfma(const T a[M][N], const T b[N][M], const T c[M][M], T res[M][M]) {
 #pragma HLS ARRAY_PARTITION variable=c complete dim=0
 #pragma HLS INTERFACE register port=res
 #pragma HLS ARRAY_PARTITION variable=res complete dim=0
-*/
+  */
   T tmp = 0;
 Rows:
   for (int i = 0; i < M; i++) {
 #pragma HLS PIPELINE
   Cols:
     for (int j = 0; j < M; j++) {
-// Uncommnt following line for scenario 2
-//#pragma HLS PIPELINE
+      // Uncomment following line for scenario 1
       tmp = c_buff[i][j];
-    // Uncommnt following line for scenario 2
+
+    // Uncomment following line for scenario 2
     // tmp = c[i][j];
     Res:
       for (int k = 0; k < N; k++) {
+        // Uncomment following line for scenario 1
         tmp += mul(a_buff[i][k], b_buff[k][j]);
-        // Uncommnt following line for scenario 2
-        // tmp += mulfma(a[i][k], b[k][j]);
+
+        // Uncomment following line for scenario 2
+        // tmp += mul(a[i][k], b[k][j]);
       }
       res[i][j] = tmp;
     }
