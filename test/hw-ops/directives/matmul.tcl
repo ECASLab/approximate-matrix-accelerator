5644ee0e08a13b841fb7967ca38b7c92b2caf65c
Luis Leon <lleon95@gmail.com>
2021-12-30 17:29:56 -0600

    Add matrix multiply with custom operators

--- test/hw-ops/directives/matmul.tcl
+++ test/hw-ops/directives/matmul.tcl
@@ -4,3 +4,16 @@
 ## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ############################################################
 set_directive_top -name $::env(TOP_FUNCTION) "$::env(TOP_FUNCTION)"
+
+# Register Mappings
+set_directive_array_partition -type complete -dim 0 "matmul_top_accel" a
+set_directive_array_partition -type complete -dim 0 "matmul_top_accel" b
+set_directive_array_partition -type complete -dim 0 "matmul_top_accel" res
+
+# Unroll and pipeline
+set_directive_pipeline "MatrixMultiply<T, M, N, ADD, MULT, NL>::Execute/ama_hw_matrix_mult_rows"
+set_directive_unroll "MatrixMultiply<T, M, N, ADD, MULT, NL>::Execute/ama_hw_matrix_mult_reduce"
+set_directive_loop_flatten -off "MatrixMultiply<T, M, N, ADD, MULT, NL>::Execute/ama_hw_matrix_mult_rows"
+
+# No-inline
+set_directive_inline -off "MatrixMultiply<T, M, N, ADD, MULT, NL>::Execute"
