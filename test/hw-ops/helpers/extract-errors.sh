a7b0fe7dd1b90a51533bc975861f824b180c8335
Luis Leon <lleon95@gmail.com>
2022-02-15 20:04:38 -0600

    Update building system to extract data

--- test/hw-ops/helpers/extract-errors.sh
+++ test/hw-ops/helpers/extract-errors.sh
@@ -10,9 +10,12 @@ rm maxerrs*
 
 for file in $(ls *.log);
 do
-  MAX_ERR=$(cat ${file} | grep "MaxErrorTh" | awk '{print $2}')
+  MIN_ERR=$(cat ${file} | grep "TestMinVal" | awk '{print $3}')
+  MAX_ERR=$(cat ${file} | grep "TestMaxVal" | awk '{print $3}')
+  MEAN_ERR=$(cat ${file} | grep "TestMeanVal" | awk '{print $3}')
+  STD_ERR=$(cat ${file} | grep "TestStdVal" | awk '{print $3}')
   ACCEL=$(echo ${file} | awk -F  "_" '{print $1}')
-  Q_R=$(echo ${file} | awk -F  "_" '{print $2}')
-  Q_BW=$(echo ${file} | awk -F  "_" '{print $3}' | sed 's/.log//g')
-  echo "$ACCEL $Q_R $Q_BW $MAX_ERR" >> maxerrs_${ACCEL}.data
+  Q_DIM=$(echo ${file} | awk -F  "_" '{print $2}')
+  Q_WL=$(echo ${file} | awk -F  "_" '{print $4}' | sed 's/.log//g')
+  echo "$ACCEL $Q_DIM $Q_WL $MIN_ERR $MAX_ERR $MEAN_ERR $STD_ERR" >> maxerrs_${ACCEL}.data
 done
